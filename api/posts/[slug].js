c790e4341b42e3ca1aa7afffef03cfb26b45a652
Marcos Henrique <marcos.henrique.a7x@gmail.com>
2026-01-14 17:31:15 -0300

    slug removido

--- api/posts/[slug].js
+++
@@ -1,8 +0,0 @@
-import { getPostBySlug } from "../../controllers/PostController.js";
-
-export default function handler(req, res) {
-  if (req.method !== "GET") {
-    return res.status(405).end();
-  }
-  return getPostBySlug(req, res);
-}
