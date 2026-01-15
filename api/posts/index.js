28059f3190ac2acebeae4a933a5298c81575e697
Marcos Henrique <marcos.henrique.a7x@gmail.com>
2026-01-14 22:33:21 -0300

    fix: cors preflight for posts api

--- api/posts/index.js
+++ api/posts/index.js
@@ -4,6 +4,20 @@ import {
 } from "../../controllers/PostController.js";
 
 export default async function handler(req, res) {
+  // 🔴 CORS preflight
+  if (req.method === "OPTIONS") {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,POST,PUT,PATCH,DELETE,OPTIONS"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+    return res.status(200).end();
+  }
+
   if (req.method === "GET") {
     return getAllPosts(req, res);
   }
