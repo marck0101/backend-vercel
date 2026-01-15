a42594103bc248ad9f559372331ebda539003280
Marcos Henrique <marcos.henrique.a7x@gmail.com>
2026-01-14 22:59:35 -0300

    fix: cors origin ad api url alignment

--- api/posts/index.js
+++ api/posts/index.js
@@ -3,18 +3,29 @@ import {
   createPost,
 } from "../../controllers/PostController.js";
 
+const ALLOWED_ORIGINS = [
+  "http://localhost:5173",
+  "https://blog.marck0101.com.br",
+];
+
 export default async function handler(req, res) {
-  // 🔴 CORS preflight
+  const origin = req.headers.origin;
+
+  if (ALLOWED_ORIGINS.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+
+  // Preflight
   if (req.method === "OPTIONS") {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET,POST,PUT,PATCH,DELETE,OPTIONS"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization"
-    );
     return res.status(200).end();
   }
 
